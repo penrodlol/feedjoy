ffb39ede09edc366a9e88268a7c4dbf108d04e9b
penrodlol <christian@penrod.contact>
2023-04-16 22:19:46 -0400

    ⚡️ reduce supabase calls (also caching doesnt seem to be working idk...)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { NextAnchor } from '@/ui/anchor';
 import Card from '@/ui/card';
 import { ArrowRight, User } from 'lucide-react';
 
+export const revalidate = 86400;
+
 async function getRoot() {
   const { data, error } = await supabase.rpc('get_root_summary').single();
   if (error) throw new Error(error.message);
@@ -21,11 +23,8 @@ async function getRecentPosts() {
   return data.map((post) => ({ ...post, site: post.site as Site }));
 }
 
-export const revalidate = 86400;
-
 export default async function Home() {
-  const { totalposts, totalsites, postweek } = await getRoot();
-  const recentPosts = await getRecentPosts();
+  const [stats, recentPosts] = await Promise.all([getRoot(), getRecentPosts()]);
 
   return (
     <div className="mx-auto my-fluid-4 flex max-w-screen-md flex-col gap-fluid-3">
@@ -37,15 +36,15 @@ export default async function Home() {
         <ul className="flex max-w-max items-center gap-fluid-4 overflow-x-auto pb-1">
           <li className="flex shrink-0 flex-col gap-x-3 sm:flex-row sm:items-center">
             <span className="text-2">total posts:</span>
-            <span>{numberFrmt.format(totalposts)}</span>
+            <span>{numberFrmt.format(stats.totalposts)}</span>
           </li>
           <li className="flex shrink-0 flex-col gap-x-3 sm:flex-row sm:items-center">
             <span className="text-2">total sites:</span>
-            <span>{numberFrmt.format(totalsites)}</span>
+            <span>{numberFrmt.format(stats.totalsites)}</span>
           </li>
           <li className="flex shrink-0 flex-col gap-x-3 sm:flex-row sm:items-center">
             <span className="text-2">posts this week:</span>
-            <span>{numberFrmt.format(postweek)}</span>
+            <span>{numberFrmt.format(stats.postweek)}</span>
           </li>
         </ul>
       </section>
