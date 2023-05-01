a87d2d98306e6599b1884fab9dabd0f0abecfbdb
penrodlol <christian@penrod.contact>
2023-04-30 22:07:26 -0400

    💄 a bit too tiny

--- src/ui/card.tsx
+++ src/ui/card.tsx
@@ -9,10 +9,9 @@ export default function Card<T extends string>(props: Props<T>) {
     <Link
       {...props}
       className="group flex h-full flex-col gap-2 rounded bg-2 p-3 pb-4 shadow
-                  transition-colors duration-150 ease-in-out text-xs hover:bg-3
-                  focus-visible:bg-3"
+                 transition-colors duration-150 ease-in-out hover:bg-3 focus-visible:bg-3"
     >
-      <div className="flex flex-wrap justify-between gap-x-4 gap-y-2 text-2">
+      <div className="flex flex-wrap justify-between gap-x-4 gap-y-2 text-2 text-xs">
         {props.children}
       </div>
       <p className="px-2">{props.title}</p>
