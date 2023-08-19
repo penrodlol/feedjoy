a81c4c52977df287e556517663fbf7011a844a3e
penrodlol <christian@penrod.contact>
2023-08-19 15:15:30 -0400

    🏗️ move const -> lib

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
 
 export type PageSchema = z.infer<typeof pageSchema>;
+export type StringSchema = z.infer<typeof stringSchema>;
 export type SlugSchema = z.infer<typeof slugSchema>;
 
 export const pageSchema = z.coerce.number().positive().int();
-export const slugSchema = z.coerce.string().uuid();
+export const stringSchema = z.string().trim();
+export const slugSchema = stringSchema.regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/);
