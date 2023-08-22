d0935aaa52804ac51324ba488b2e128bad600fcd
penrodlol <christian@penrod.contact>
2023-08-22 19:52:20 -0400

    💄 minor styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         <h2 className="text-lg tracking-wider text-2">a minimal RSS feed aggregator</h2>
       </section>
       <Separator />
-      <section className="flex max-w-prose flex-col gap-6 text-sm !leading-8 text-2">
+      <section className="flex flex-col gap-6 text-sm !leading-8 text-2">
         <p>
           feedjoy aggregates blog posts from multiple RSS feeds into a single source. Its goal is to
           provide a more efficient way of tracking new posts without having to subscribe to multiple
@@ -16,10 +16,10 @@ export default function Page() {
         </p>
         <p>
           On a daily basis, feedjoy will fetch any new posts across the list of provided RSS feeds
-          <em className="px-1 text-xs">(maintained internally)</em>. These new posts are then passed
-          through GPT to generate a short summary and identify the most relevant topic. The goal
-          here is to identify trends and quickly consume content without having to navigate
-          externally.
+          <em className="whitespace-nowrap px-1 text-xs">(maintained internally)</em>. These new
+          posts are then passed through GPT to generate a short summary and identify the most
+          relevant topic. The goal here is to identify trends and quickly consume content without
+          having to navigate externally.
         </p>
       </section>
     </div>
